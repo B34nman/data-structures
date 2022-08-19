028abbbb6285c8694a9f26e3ab32303b82788174
Andy-Home <1amajfranck@gmail.com>
2022-08-19 16:59:46 -0700

    Did lab 10

--- 163/Labs/Lab10/cs163_lab10.cpp
+++ 163/Labs/Lab10/cs163_lab10.cpp
@@ -8,6 +8,28 @@ int main()
     trees.display();
 
     /*  PLACE YOUR FUNCTION CALL HERE */
+    trees.display_largest();
+    int rootCount = trees.count_root();
+    cout << "Root count: " << rootCount << endl;
+    cout << endl << endl;
+
+    table treeCopy;
+    treeCopy.copy(trees);
+    treeCopy.display();
+    
+    cout << endl << endl;
+
+    int count = trees.count_3_data();
+    cout << "Count of 3 data nodes: " << count << endl;
+    cout << endl;
+
+    int height = trees.height();
+    cout << "Height of 2-3-4 tree: " << height << endl;
+    cout << endl;
+
+    table treeCopy2;
+    treeCopy2.copy_234(trees);
+    treeCopy2.display();
 
     return 0;
 }
--- 163/Labs/Lab10/cs163_tree.cpp
+++ 163/Labs/Lab10/cs163_tree.cpp
@@ -6,14 +6,50 @@ using namespace std;
 //These are the functions that you will be writing for the BST
 
 
-void table::display_largest(){}      	//STEP 1
-void table::display_largest(node * root) {}
+void table::display_largest()
+{
+      display_largest(root);
+      return;
+}      	//STEP 1
+void table::display_largest(node * root)
+{
+      if(root == NULL)
+            return;
+      if(root->right == NULL)
+            cout << root->data << endl;
 
-int table::count_root() {}		//STEP 2
-int table::count_root(node * root, int match) {}
+      display_largest(root->right);
+}
 
-void table::copy(table & source) {}  	//STEP 3
-void table::copy(node * & destination, node * source) {}
+int table::count_root()
+{
+      return count_root(root, root->data);
+}	//STEP 2
+int table::count_root(node * root, int match)
+{
+      if(!root)
+            return 0;
+      
+      if(match == root->data)
+            return 1 + count_root(root->left, match) + count_root(root->right, match);
+      else
+            return count_root(root->left, match) + count_root(root->right, match);
+}
+
+void table::copy(table & source)
+{
+      copy(root, source.root);
+}	//STEP 3
+
+void table::copy(node * & destination, node * source)
+{
+      if(!source)
+            return;
+      destination = new node;
+      destination->data = source->data;
+      copy(destination->left, source->left);
+      copy(destination->right, source->right);
+}
 
 
 
@@ -23,14 +59,60 @@ int table::count_3_data()   //sample wrapper function that return the count
       return count_3_data(root234);
 }	     
      
-int table::count_3_data(node234 * root){}	                    
-
+int table::count_3_data(node234 * root)
+{
+      if(!root)
+            return 0;
+      int count = 0;
+      if(root->data[2])
+            ++count;
+      if(root->data[1])
+            ++count;
+      if(root->data[0])
+            ++count;
+      return count + count_3_data(root->child[0])
+             + count_3_data(root->child[1])
+             + count_3_data(root->child[2])
+             + count_3_data(root->child[3]);
+}
 
 //STEP 5
-int table::height(){}                    //Return the height
-int table::height(node234 * root){}                    
+int table::height()
+{
+      return height(root234);
+}
+int table::height(node234 * root)
+{
+      if(!root)
+            return 0;
+      int max = 0;
+      for(int i = 0; i < 4; ++i)
+      {
+            int temp = height(root->child[i]);
+            if(temp > max)
+                  max = temp;
+      }
+      return max + 1; 
+}
 
 //STEP 6
 //Copy from the argument into the data members...
-int table::copy_234(table & copy_from) {}
-int table::copy(node234 * & destination, node234 * source){}      
+int table::copy_234(table & copy_from) 
+{
+      return copy(root234, copy_from.root234);
+}
+int table::copy(node234 * & destination, node234 * source)
+{
+      if(!source)
+            return 0;
+      destination = new node234;
+      destination->data[0] = source->data[0];
+      destination->data[1] = source->data[1]; 
+      destination->data[2] = source->data[2]; 
+      destination->data[3] = source->data[3];
+      copy(destination->child[0], source->child[0]);
+      copy(destination->child[1], source->child[1]);
+      copy(destination->child[2], source->child[2]);
+      copy(destination->child[3], source->child[3]);
+      return 1;  
+}
